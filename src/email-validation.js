91731b94b99efc38d3799ba26a0382f6315a11b6
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-08-22 12:52:23 +0530

    Create email-validation.js
    Target email input and email span elements
    Create validateEmail function
    Create emailValidation function
    
    Additional changes:
    - Add error class to email span element
    - Add css styling to error class
    - import validateEmail function to index.ls

---
+++ src/email-validation.js
@@ -0,0 +1,20 @@
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#email + span");
+
+export function validateEmail() {
+  email.addEventListener("input", () => {
+    if (!email.validity.valid) {
+      emailValidation();
+    } else {
+      emailError.textContent = "";
+    }
+  });
+}
+
+function emailValidation() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "Email is required";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Enter valid email";
+  }
+}
